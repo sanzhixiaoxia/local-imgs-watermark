38db455088861099048c0ba9439bc3ae11f3f5b5
sanzhixiaoxia <sanzhixiaoxia@users.noreply.github.com>
2026-07-23 02:02:47 +0000

    feat: 配置 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/local-imgs-watermark/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
